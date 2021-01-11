99179a5a583e8e1d8763cf9a2dedc89d77967e8d
Lukas Bombach <lukas.bombach@googlemail.com>
2021-01-11 22:24:52 +0100

    got the split

--- src/parseEmail.ts
+++ src/parseEmail.ts
@@ -1,6 +1,8 @@
 import { promises as fs } from "fs";
 import { inspect } from "util";
 
+type ContextType = "root" | "header" | "multipart";
+
 function log(value: any) {
   console.log(inspect(value, { depth: 10, colors: true }));
 }
@@ -9,7 +11,8 @@ const regex = {
   responseHeader: /^\* (?<id>\d+) FETCH \((?<flags>.+?) \{(?<length>\d+)\}$/,
   header: /^(?<key>[!-9;-~]+?):(?<value>.*)/,
   foldingWhiteSpace: /^ (?<value>.*)/,
-  multipartBoundary: /^--(?<value>.*)/,
+  multipartBoundary: /^--(?<value>.*)(?!--)$/,
+  multipartClose: /^--(?<value>.*)--$/,
 };
 
 function parseLines(lines: string[]) {
@@ -18,33 +21,45 @@ function parseLines(lines: string[]) {
   for (const line of lines) {
     const lastLine = parsedLines[parsedLines.length - 1];
 
-    if (
-      lastLine &&
-      lastLine.type === "multipartBoundary" &&
-      lastLine.closed === false
-    ) {
-      if (regex.multipartBoundary.test(line)) {
+    if (lastLine?.type === "multipartBoundary" && !lastLine?.closed) {
+      if (regex.multipartClose.test(line)) {
+        log("closing the multipart");
+        console.log(line);
         lastLine.closed = true;
+      } else if (regex.multipartBoundary.test(line)) {
+        log("splitting the multipart");
+        lastLine.values.push("");
+        console.log(line);
       } else {
-        lastLine.value += line;
+        log("adding to the multipart");
+        console.log(line);
+        lastLine.values[lastLine.values.length - 1] += `\n${line}`;
       }
     } else if (regex.multipartBoundary.test(line)) {
+      log("opening a mutlipart");
+      console.log(line);
       parsedLines.push({
         type: "multipartBoundary",
-        ...line.match(regex.multipartBoundary).groups,
+        values: [""],
         closed: false,
       });
     } else if (regex.responseHeader.test(line)) {
+      log("responseHeader");
+      console.log(line);
       parsedLines.push({
         type: "responseHeader",
         ...line.match(regex.responseHeader).groups,
       });
     } else if (regex.header.test(line)) {
+      log("header");
+      console.log(line);
       parsedLines.push({
         type: "header",
         ...line.match(regex.header).groups,
       });
     } else if (regex.foldingWhiteSpace.test(line)) {
+      log("header append");
+      console.log(line);
       parsedLines[parsedLines.length - 1].value += line.match(
         regex.foldingWhiteSpace
       ).groups.value;
@@ -59,21 +74,25 @@ async function main() {
     const filePath = `${__dirname}/max.email.txt`;
     const text = await fs.readFile(filePath, "utf-8");
     const lines = text.split(/\r?\n/);
-    const parsedLines = parseLines(lines);
+    const parsed = parseLines(lines);
+    log(parsed);
 
-    /* const parsedLines = lines
-      .map(line => {
-        if (/^\* 1 FETCH/.test(line)) {
-          const r = /^\* (?<id>\d+) FETCH \((?<flags>.+?) \{(?<length>\d+)\}$/;
-          return { type: "start", ...line.match(r).groups };
-        } else if (/^(?<key>[!-9;-~]+?): (?<value>.*)/.test(line)) {
-          const r = /^(?<key>[!-9;-~]+?): (?<value>.*)/;
-          return { type: "header", ...line.match(r).groups };
+    /* 
+    const parsedLines: any[] = [];
+
+    let context: { type: ContextType } & Record<string, any> = { type: "root" };
+
+    function add(type: keyof typeof regex, data: Record<string, string>) {
+      parsedLines.push({ type, ...data });
+    } */
+
+    /* for (const line of lines) {
+      if (context.type === "root") {
+        if (regex.responseHeader.test(line)) {
+          add("responseHeader", line.match(regex.responseHeader).groups);
         }
-      })
-      .filter(Boolean); */
-
-    log(parsedLines);
+      }
+    } */
   } catch (error) {
     console.error(error);
   }
