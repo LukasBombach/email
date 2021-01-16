816b0d18d0ee5c7009c3cb25f0e3f0f1a772c078
Lukas Bombach <lukas.bombach@googlemail.com>
2021-01-16 21:46:23 +0100

    complete yet initial parsed message

--- src/parseEmail.ts
+++ src/parseEmail.ts
@@ -1,41 +1,126 @@
 import { promises as fs } from "fs";
 import { inspect } from "util";
 
-interface Message {
-  flags: string;
-  length: number;
-  headers: Record<string, string | number>;
-  bodies: string[];
-}
+import type { InspectOptions } from "util";
 
-function log(value: any) {
-  console.log(inspect(value, { depth: 10, colors: true }));
-}
+type EmailHeaders = Record<string, string>;
+type Groups = Record<string, string>;
 
-const regex = {
-  responseHeader: /^\* (?<id>\d+) FETCH \((?<flags>.+?) \{(?<length>\d+)\}$/,
-  header: /^(?<key>[!-9;-~]+?):(?<value>.*)/,
-  foldingWhiteSpace: /^ (?<value>.*)/,
-  multipartBoundary: /^--(?<value>.*)(?!--)$/,
-  multipartClose: /^--(?<value>.*)--$/,
-};
-
-async function main() {
+(async function main() {
   try {
     const filePath = `${__dirname}/max.email.txt`;
-    const text = await fs.readFile(filePath, "utf-8");
+    const message = await fs.readFile(filePath, "utf-8");
 
-    const secondLine = text.indexOf("\n");
-    const split = text.indexOf("\n\n");
-    const lastLine = text.lastIndexOf("\n)\n");
+    const { head, headers, body } = parseMessage(message);
 
-    const headers = text.substr(secondLine, split - secondLine);
-    const body = text.substr(split, lastLine - split);
-
-    console.log(headers);
+    log(head);
+    log(headers);
+    log(body, { maxStringLength: null });
   } catch (error) {
     console.error(error);
   }
+})();
+
+function log(value: any, options: InspectOptions = {}) {
+  console.log(
+    inspect(value, {
+      depth: 10,
+      colors: true,
+      compact: false,
+      maxStringLength: 80,
+      ...options,
+    })
+  );
 }
 
-main();
+const EOL = "\n";
+
+function splitMessage(
+  message: string
+): { head: string; headers: string; body: string } {
+  const secondLine = message.indexOf(EOL);
+  const split = message.indexOf(`${EOL}${EOL}`);
+  const lastLine = message.lastIndexOf(`${EOL})${EOL}`);
+
+  const head = message.substring(0, secondLine);
+  const headers = message.substring(secondLine, split);
+  const body = message.substring(split, lastLine);
+
+  return { head, headers, body };
+}
+
+function parseMessage(message: string) {
+  const messageParts = splitMessage(message);
+  const head = parseHead(messageParts.head);
+  const headers = parseHeaders(messageParts.headers);
+  const body = parseBody(messageParts.body);
+
+  return { head, headers, body };
+}
+
+function parseHead(head: string) {
+  const regex = /^\* (?<id>\d+) FETCH \((?<flags>.+?) \{(?<length>\d+)\}$/;
+  return getGroups<"id" | "flags" | "length">(head, regex);
+}
+
+function parseHeaders(headers: string) {
+  const header = /^(?<key>[^ ][!-9;-~]+?):(?<value>.*)/;
+  const fold = /^ (?<value>.*)/;
+  const lines = headers.split(EOL);
+
+  const parsedLines = lines.reduce<{ key: string; value: string }[]>(
+    (headers, line) => {
+      if (!line) {
+        return headers;
+      }
+      if (test(line, fold)) {
+        headers[headers.length - 1].value += getGroups<"value">(
+          line,
+          fold
+        ).value;
+        return headers;
+      }
+      if (test(line, header)) {
+        return [...headers, getGroups<"key" | "value">(line, header)];
+      }
+      throw new Error(`Failed to parse "${line}"`);
+    },
+    []
+  );
+  return parsedLines.reduce<EmailHeaders>(
+    (headers, { key, value }) => Object.assign(headers, { [key]: value }),
+    {}
+  );
+}
+
+function parseBody(body: string) {
+  const structure = /\n--(?<boundary>.*?)\n(?<content>.*?)\n--\1--/s;
+  const { boundary, content } = getGroups<"boundary" | "content">(
+    body,
+    structure
+  );
+  const parts = content.split(new RegExp(`\n--${boundary}\n`));
+  //return parts[0];
+  return parts.map(part => {
+    const split = part.indexOf(`${EOL}${EOL}`);
+    const headers = parseHeaders(part.substring(0, split));
+    const body = part.substring(split).trim();
+    return { headers, body };
+  });
+}
+
+function test(str: string, regex: RegExp): boolean {
+  const result = regex.test(str);
+  regex.lastIndex = 0;
+  return result;
+}
+
+function getGroups<T extends string = string>(
+  str: string,
+  regex: RegExp
+): { [key in T]: string } {
+  const result = str.match(regex);
+  if (!result?.groups) throw new Error(`Could not match ${regex} to "${str}"`);
+  regex.lastIndex = 0;
+  return result.groups as { [key in T]: string };
+}
