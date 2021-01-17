dccb879badf2d3f5ea2819be31d129df3c4a088f
Lukas Bombach <lukas.bombach@googlemail.com>
2021-01-17 22:05:57 +0100

    body model

--- src/parse/body.ts
+++ src/parse/body.ts
@@ -1,13 +1,24 @@
 import { getGroups, parseHeaders, EOL } from ".";
 import { decode } from "content-encodings";
 
+interface ParsedPart {
+  headers: Record<string, string>;
+  body: string;
+}
+
+export interface Body {
+  text?: string;
+  html?: string;
+}
+
 export function parseBody(body: string) {
   const { content, boundary } = getStructure(body);
   const parts = getMultiparts(content, boundary);
-  return parts.map(part => parseMultipart(part));
+  const parsedParts = parts.map(part => parseMultipart(part));
+  return createBody(parsedParts);
 }
 
-export function parseMultipart(part: string) {
+export function parseMultipart(part: string): ParsedPart {
   const split = part.indexOf(`${EOL}${EOL}`);
   const headers = parseHeaders(part.substring(0, split));
   const body = decode("qp", part.substring(split).trim()).toString();
@@ -21,4 +32,19 @@ function getStructure(body: string) {
 
 function getMultiparts(content: string, boundary: string) {
   return content.split(new RegExp(`\n--${boundary}\n`));
+}
+
+function createBody(parsedParts: ParsedPart[]): Body {
+  const text = findBody(parsedParts, "text/plain");
+  const html = findBody(parsedParts, "text/html");
+  return { text, html };
+}
+
+function findBody(parsedParts: ParsedPart[], contentType: string) {
+  const part = parsedParts.find(part => hasContentType(part, contentType));
+  return part ? part.body : undefined;
+}
+
+function hasContentType(part: ParsedPart, contentType: string) {
+  return part.headers["Content-Type"]?.includes(contentType);
 }
